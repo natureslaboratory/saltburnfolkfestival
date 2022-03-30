27e269fb0853d5b15ed447ed7990d4e3a82a2f50
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-30 09:22:38 +0100

    Update global.footer.php

--- perch/templates/layouts/global.footer.php
+++ perch/templates/layouts/global.footer.php
@@ -11,7 +11,7 @@
         </div> -->
         <?php perch_content_create("Social Icons", [ "template" => "socials.html", "data" => [ "type" => "c-footer__socials" ] ]); ?>
         <?php perch_content_custom("Social Icons") ?>
-        <?php perch_content('Footer'); ?>
+        <?php perch_content('Footer Text'); ?>
         <?php perch_content_create("Footer Links", [ "template" => "legal.html" ]) ?>
         <?php perch_content_custom("Footer Links"); ?>
     </div>
